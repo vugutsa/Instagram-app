09a1b72ff40010533c480c1fa9b771ae52cdefc2
vugutsa <vugutsamercy84@gmail.com>
2021-01-16 20:52:26 +0300

    add the app configuration news.apps.NewsConfiginside our INSTALLED_APPS

--- insta/settings.py
+++ insta/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gramm.apps.GrammConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
