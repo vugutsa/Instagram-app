b30f2413940a8f0724ecec677dbd3c78f579393e
vugutsa <vugutsamercy84@gmail.com>
2021-01-17 00:45:00 +0300

    add a view function of getting date for the insta

--- gramm/views.py
+++ gramm/views.py
@@ -1,5 +1,50 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
+import datetime as dt
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to Instagram')
+
+def insta_today(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Instas for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return render(request, 'all-gramm/today-gramm.html', {"date": date,})
+
+# def convert_dates(dates):
+    
+#     # Function that gets the weekday number for the date.
+#     day_number = dt.date.weekday(dates)
+
+#     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+#     # Returning the actual day of the week
+#     day = days[day_number]
+#     return day
+
+# def past_days_insta(request,past_date):
+#     try:
+#     # Converts data from the string Url
+#         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+#     except ValueError:
+#         # Raise 404 error when ValueError is thrown
+#         raise Http404()
+#     day = convert_dates(date)
+#     html = f'''
+#         <html>
+#             <body>
+#                 <h1>Insta for {day} {date.day}-{date.month}-{date.year}</h1>
+#             </body>
+#         </html>
+#             '''
+#     if date == dt.date.today():
+#         return redirect(news_of_day)    
+#     return render(request, 'all-gramm/past-gramm.html', {"date": date})
