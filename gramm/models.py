a5b01576cc4aeead38d1d8edf785c8e6e9c14df8
vugutsa <vugutsamercy84@gmail.com>
2021-01-17 01:20:26 +0300

    create the django admin

--- gramm/models.py
+++ gramm/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length =60)
+    description = models.CharField(max_length =200)
+    date = models.DateTimeField(auto_now_add=True)
+    Images_image = models.ImageField(upload_to = 'images/')
+    # profile = models.ForeignKey('profile', on_delete=models.CASCADE)
+
