a6a9d93e6c8a7ba7e1a36b300e1b7425d7b33dc1
vugutsa <vugutsamercy84@gmail.com>
2021-01-20 03:09:59 +0300

    deployment

--- insta/settings.py
+++ insta/settings.py
@@ -17,11 +17,7 @@ from decouple import config,Csv
 
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORDf')
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
